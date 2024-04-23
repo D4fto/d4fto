acc86fc7d7153d06fafbeaa3e8861d7a0ebec861
D4fto <163424876+D4fto@users.noreply.github.com>
2024-04-22 22:46:50 -0300

    update epic gomes

--- epic_gomes/script.js
+++ epic_gomes/script.js
@@ -2,8 +2,36 @@ const conta_button = document.getElementById("conta_button")
 const login_check = document.getElementById("loggin")
 const search = document.getElementById("procura")
 const search_form = document.getElementById("pesquisa")
+const preco_slider = document.getElementById("preco")
 var login = false
 
+function init(){
+    for(let i=0; i<document.getElementsByClassName("filtros").length; i++){
+        if(document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="1fr"){
+            document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="1fr"
+        }
+        else if(!document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="0fr"){
+            document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="0fr"
+    
+        }
+    }
+    if(preco_slider.value == preco_slider.max){
+        document.getElementById("preco_output").style.display='none'
+        document.getElementById("qualquer").style.display='block'
+        document.getElementById("gratis").style.display='none'
+    }
+    else if(preco_slider.value == preco_slider.min){
+        document.getElementById("preco_output").style.display='none'
+        document.getElementById("gratis").style.display='block'
+    }
+    else{
+        document.getElementById("preco_output").style.display='block'
+        document.getElementById("qualquer").style.display='none'
+        document.getElementById("gratis").style.display='none'
+    }
+
+}
+init()
 conta_button.addEventListener("click",function(){
     login = login_check.checked
     if(login){
@@ -14,6 +42,42 @@ conta_button.addEventListener("click",function(){
     }
 })
 procura.addEventListener("input",function(){
-    console.log(procura.value)
-    search_form.action="https://store.steampowered.com/search/?term="+search.value+"&supportedlang=brazilian&ndl=1"
+    
+    search_form.action="https://store.steampowered.com/search/?term="+search.value+"&ndl=1"
+})
+for(let i=0; i<document.getElementsByClassName("filtros").length; i++){
+    if(document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="1fr"){
+        document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="1fr"
+    }
+    else if(!document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="0fr"){
+        document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="0fr"
+
+    }
+}
+document.addEventListener("input",function(){
+    for(let i=0; i<document.getElementsByClassName("filtros").length; i++){
+        if(document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="1fr"){
+            document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="1fr"
+        }
+        else if(!document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="0fr"){
+            document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="0fr"
+
+        }
+    }
+})
+preco_slider.addEventListener("input",function(){
+    if(preco_slider.value == preco_slider.max){
+        document.getElementById("preco_output").style.display='none'
+        document.getElementById("qualquer").style.display='block'
+        document.getElementById("gratis").style.display='none'
+    }
+    else if(preco_slider.value == preco_slider.min){
+        document.getElementById("preco_output").style.display='none'
+        document.getElementById("gratis").style.display='block'
+    }
+    else{
+        document.getElementById("preco_output").style.display='block'
+        document.getElementById("qualquer").style.display='none'
+        document.getElementById("gratis").style.display='none'
+    }
 })
