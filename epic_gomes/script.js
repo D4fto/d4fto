4de64b5c6111af4fb5360f4b2257a036e83289dd
D4fto <163424876+D4fto@users.noreply.github.com>
2024-04-23 22:50:52 -0300

    update

--- epic_gomes/script.js
+++ epic_gomes/script.js
@@ -3,18 +3,22 @@ const login_check = document.getElementById("loggin")
 const search = document.getElementById("procura")
 const search_form = document.getElementById("pesquisa")
 const preco_slider = document.getElementById("preco")
+const filtros = document.getElementsByClassName("filtro")
+const menu_vertical = document.getElementsByClassName("menu_vertical")
 var login = false
-
-function init(){
+var generos = ""
+function filtro_collapsable_chec(){
     for(let i=0; i<document.getElementsByClassName("filtros").length; i++){
         if(document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="1fr"){
             document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="1fr"
         }
         else if(!document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="0fr"){
             document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="0fr"
-    
+
         }
     }
+}
+function preco_slider_custom(){
     if(preco_slider.value == preco_slider.max){
         document.getElementById("preco_output").style.display='none'
         document.getElementById("qualquer").style.display='block'
@@ -29,7 +33,11 @@ function init(){
         document.getElementById("qualquer").style.display='none'
         document.getElementById("gratis").style.display='none'
     }
-
+}
+function init(){
+    filtro_collapsable_chec()
+    preco_slider_custom()
+    filtros_chec()
 }
 init()
 conta_button.addEventListener("click",function(){
@@ -43,7 +51,7 @@ conta_button.addEventListener("click",function(){
 })
 procura.addEventListener("input",function(){
     
-    search_form.action="https://store.steampowered.com/search/?term="+search.value+"&ndl=1"
+    search_form.action="https://store.steampowered.com/search/?term="+search.value+generos+"&ndl=1"
 })
 for(let i=0; i<document.getElementsByClassName("filtros").length; i++){
     if(document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="1fr"){
@@ -55,29 +63,28 @@ for(let i=0; i<document.getElementsByClassName("filtros").length; i++){
     }
 }
 document.addEventListener("input",function(){
-    for(let i=0; i<document.getElementsByClassName("filtros").length; i++){
-        if(document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="1fr"){
-            document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="1fr"
-        }
-        else if(!document.getElementsByClassName("filtros")[i].checked & document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows!="0fr"){
-            document.getElementById(document.getElementsByClassName("filtros")[i].id+"_container").style.gridTemplateRows="0fr"
-
-        }
-    }
+    
 })
 preco_slider.addEventListener("input",function(){
-    if(preco_slider.value == preco_slider.max){
-        document.getElementById("preco_output").style.display='none'
-        document.getElementById("qualquer").style.display='block'
-        document.getElementById("gratis").style.display='none'
+    preco_slider_custom()
+    
+})
+function filtros_chec(){
+    generos=""
+    for(let i=0; i<filtros.length; i++){
+        if(filtros[i].classList.contains("gen_filtro")){
+            if(filtros[i].checked){
+                if(generos==""){
+                    generos+="&tags="+filtros[i].name
+                }
+                else{
+                    generos+="%2C"+filtros[i].name
+                }
+            }
+        }
     }
-    else if(preco_slider.value == preco_slider.min){
-        document.getElementById("preco_output").style.display='none'
-        document.getElementById("gratis").style.display='block'
-    }
-    else{
-        document.getElementById("preco_output").style.display='block'
-        document.getElementById("qualquer").style.display='none'
-        document.getElementById("gratis").style.display='none'
-    }
+}
+menu_vertical[0].addEventListener("input", function(){
+    filtros_chec()
+    filtro_collapsable_chec()
 })
