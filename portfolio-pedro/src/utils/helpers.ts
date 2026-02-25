7c809ebd2323b3b80a6eadf2d2d806b7ddd18f8f
D4fto <163424876+D4fto@users.noreply.github.com>
2026-02-25 13:47:29 -0300

    Update helpers.ts

--- portfolio-pedro/src/utils/helpers.ts
+++ portfolio-pedro/src/utils/helpers.ts
@@ -36,16 +36,16 @@ export const getOrCreateUserId = (): string => {
 
 export const sendAnalytics = async (): Promise<void> => {
   try {
-    // await fetch(import.meta.env.VITE_WEBHOOK, {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({
-    //     content: `📊 Acessaram meu portfólio
-    //     🕒 ${new Date().toLocaleString()}
-    //     🆔 ${getOrCreateUserId()}
-    //     ----------------------------------`,
-    //   }),
-    // });
+    await fetch(import.meta.env.VITE_WEBHOOK, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        content: `📊 Acessaram meu portfólio
+        🕒 ${new Date().toLocaleString()}
+        🆔 ${getOrCreateUserId()}
+        ----------------------------------`,
+      }),
+    });
   } catch (error) {
     console.error('Analytics error:', error);
   }
